dcfce55c2418efb220a13f4e2105d8d44547455b
dustin-kang <dongwoomail@icloud.com>
2021-12-12 22:10:06 +0900

    Flask Local Complete

---
+++ mbtify/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from music import *
+from secrets import *
+
+with open('model.pkl','rb') as pickle_file:
+   model = pickle.load(pickle_file)
+
+mbtify = Flask(__name__) #어플리케이션 이름 지정
+
+@mbtify.route('/')
+def send():
+    # root url인 ('/')로 접근 했을 경우 send.html를 렌더링한다.
+    return render_template('send.html')
+
+
+@mbtify.route('/recv', methods=['POST', 'GET']) # 받을 경우
+def recv():
+    data = request.form['inpute_code']
+
+    info, features = getTrackFeatures(token, data)
+    pred = model.predict(features)
+
+    artist = info['artist']
+    url = info['image_url']
+    song = info['song']
+    # 데이터 API 가져와 변환하기
+    return render_template('recv.html', pred=pred, artist=artist, url=url, song=song)
+
+@mbtify.errorhandler(404) # 페이지 오류
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+
+if __name__ == '__main__': # Debug Mode ON
+    app.run(debug=True)
--- mbtify/routes/user_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('user', __name__, url_prefix='/result')
-
-@bp.route('/', methods=['POST','GET'])
-def index():
-    return render_template('result.html')
