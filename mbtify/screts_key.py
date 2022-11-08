077c0c60225f8663b6c6647b87cbc3b117249f04
dustin-kang <dongwoomail@icloud.com>
2022-11-08 20:56:34 +0900

    Delete mbtify directory

--- mbtify/screts_key.py
+++
@@ -1,9 +0,0 @@
-clientID = "985bddba809c4d149f1347b928dcc4a4"
-clientSecret = "f103e7c1f7e9436fbefb40e94943c52d"
-
-
-# Spotify user_information
-# user_id = "annieboyse"
-# user_id = "11177981386"
-
-mbti_list = ['enfj', 'enfp', 'entj' ,'entp','esfj' ,'esfp', 'estj', 'estp' ,'infj', 'infp' ,'intj', 'intp' ,'isfj', 'isfp', 'istj' ,'istp']
